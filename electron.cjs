3d1c7b7706d389a36a31a0a510a437f2091cad3c
Mukesh Sankhla <mukeshdiy1@gmail.com>
2025-06-23 13:49:25 +0530

    Initial Commit

--- electron.cjs
+++ electron.cjs
@@ -4,8 +4,9 @@ const isDev = !app.isPackaged;
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1280,
-    height: 800,
+    fullscreen: true,         // Launch in fullscreen
+    frame: false,             // Removes the top bar (title bar and borders)
+    kiosk: true,              // Locks the app in fullscreen mode (even disables ALT+F4, ESC, etc. on Windows)
     webPreferences: {
       contextIsolation: true
     }
@@ -23,3 +24,13 @@ function createWindow() {
 }
 
 app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  // On Windows, close the app when all windows are closed
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  // On macOS, recreate a window in the app when the dock icon is clicked
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
